120aa55f6c61f1469156e19c52287046d25f92e6
Joe <jemsamano@yahoo.com>
2020-06-21 16:25:59 -0600

    Added parallax effect

---
+++ js/parallax.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    parallax();
+})
+
+function parallax() {
+
+    var wScroll = $(window).scrollTop();
+
+    $(' .parallax--bg').css('background-position', 'center ' + (wScroll * 0.75) + 'px')
+
+}
