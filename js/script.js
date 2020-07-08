8b62bba5ed6495b057f1c9a492c81d95f7849ccf
Joe <jemsamano@yahoo.com>
2020-07-07 21:21:22 -0600

    Code Refactor and Added Force Scroll Top on Reload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 
+/* Adding observers and effects on scroll */
 window.onload = function(){
-    const appearOptions = {
-        rootMargin: "0px 0px -350px 0px"
-    };
-
+    
+    /* Add observers to each sections with class slide-in and to add class appear on scroll to produce effect
+       add class active to update side menu  */
     const sliders = document.querySelectorAll(".slide-in");
+    const appearOptions = {
+      rootMargin: "-200px 0px -200px 0px"
+    };
 
     const appearOnScroll = new IntersectionObserver(function(
         entries,
@@ -12,22 +15,41 @@ window.onload = function(){
     ) {
         entries.forEach(entry => {
         if (!entry.isIntersecting) {
+          var i;
+          for(i = 1; i < 6; i++) {
+            $('#link' + i).removeClass("active");
+          }
             return;
         } else {
             entry.target.classList.add("appear");
-            appearOnScroll.unobserve(entry.target);
+            console.log(entry.target.className);
+            if(entry.target.classList.contains('whoami')) {
+              $('#link1').addClass("active");
+            }
+            else if(entry.target.classList.contains('backstory')) {
+              $('#link2').addClass("active");
+            }
+            else if(entry.target.classList.contains('skillset')) {
+              $('#link3').addClass("active");
+            }
+            else if(entry.target.classList.contains('interest')) {
+              $('#link4').addClass("active");
+            }
+            else if(entry.target.classList.contains('future')) {
+              $('#link5').addClass("active");
+            }
         }
         });
     },
     appearOptions);
-    
 
     sliders.forEach(slider => {
         appearOnScroll.observe(slider);
     });
 
-    const appearAfterOptions = {}
+    /* Add observers for sections with class slide-after and to add class appearAfter on scroll to produce effect */
     const slidersAfter = document.querySelectorAll(".slide-after");
+    const appearAfterOptions = {}
 
     const appearOnScrollAfter = new IntersectionObserver(function(
         entries,
@@ -38,244 +60,137 @@ window.onload = function(){
             return;
         } else {
             entry.target.classList.add("appearAfter");
-            appearOnScroll.unobserve(entry.target);
         }
         });
     },
     appearOptions);
-    
 
     slidersAfter.forEach(sliderAfter => {
         appearOnScrollAfter.observe(sliderAfter);
     });
 
-    const sectionOne = document.querySelector(".whoami");
-    const sectionTwo = document.querySelector(".backstory");
-    const sectionThree = document.querySelector(".skillset");
-    const sectionFour = document.querySelector(".interest");
-    const sectionFive = document.querySelector(".future");
-
-    const sectionOneOptions = {rootMargin: "-200px 0px -200px 0px"};
-    const sectionTwoOptions = {rootMargin: "-200px 0px -200px 0px"};
-    const sectionThreeOptions = {rootMargin: "-200px 0px -200px 0px"};
-    const sectionFourOptions = {rootMargin: "-200px 0px -200px 0px"};
-    const sectionFiveOptions = {rootMargin: "-200px 0px -200px 0px"};
-
-    const sectionOneObserver = new IntersectionObserver(function(
-        entries,
-        sectionOneObserver
-      ) {
-        entries.forEach(entry => {
-          if (!entry.isIntersecting) {
-            $('#link1').removeClass("active");
-          } else {
-            $('#link1').addClass("active");
-          }
-        });
-      },
-      sectionOneOptions);
-
-      const sectionTwoObserver = new IntersectionObserver(function(
-        entries,
-        sectionTwoObserver
-      ) {
-        entries.forEach(entry => {
-          if (!entry.isIntersecting) {
-            $('#link2').removeClass("active");
-          } else {
-            $('#link2').addClass("active");
-          }
-        });
-      },
-      sectionTwoOptions);
-
-      const sectionThreeObserver = new IntersectionObserver(function(
-        entries,
-        sectionThreeObserver
-      ) {
-        entries.forEach(entry => {
-          if (!entry.isIntersecting) {
-            $('#link3').removeClass("active");
-          } else {
-            $('#link3').addClass("active");
-          }
-        });
-      },
-      sectionThreeOptions);
-
-      const sectionFourObserver = new IntersectionObserver(function(
-        entries,
-        sectionFourObserver
-      ) {
-        entries.forEach(entry => {
-          if (!entry.isIntersecting) {
-            $('#link4').removeClass("active");
-          } else {
-            $('#link4').addClass("active");
-          }
-        });
-      },
-      sectionFourOptions);
-
-      const sectionFiveObserver = new IntersectionObserver(function(
-        entries,
-        sectionFiveObserver
-      ) {
-        entries.forEach(entry => {
-          if (!entry.isIntersecting) {
-            $('#link5').removeClass("active");
-          } else {
-            $('#link5').addClass("active");
-          }
-        });
-      },
-      sectionFiveOptions);
-    
-
-    sectionOneObserver.observe(sectionOne);
-    sectionTwoObserver.observe(sectionTwo);
-    sectionThreeObserver.observe(sectionThree);
-    sectionFourObserver.observe(sectionFour);
-    sectionFiveObserver.observe(sectionFive);
-
-
 }
 
-
-$(window).scroll(function() {
-    parallax();
-})
-
-/* Parallax */
-function parallax() {
-
-    var wScroll = $(window).scrollTop();
-
-    $(' .parallax--bg').css('background-position', 'center ' + (wScroll * 0.75) + 'px')
-
-}
+/* When refresh, move to the top of the page */
+$(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+});
 
 $(document).ready(function(){
-
-    var audio = document.getElementById("audio");
-    audio.volume = 0.3;
-
+    /* Adding Sounds to Music Logo */
     $('#music').click(function() {
         $('#music-menu').toggle();
-        var select = new Audio();
-        select.src = "audio/select.mp3"
-        select.play();
+        selectSound();
     })
 
     $("#music").mouseenter(function() {
-        var bleep = new Audio();
-        bleep.src = "audio/button.mp3"
-        bleep.play();
+        hoverSound();
     })
 
+    /* Change Volume of Background Music */
+    var audio = document.getElementById("audio");
+    audio.volume = 0.3;
+
+    /* Adding Background Music and Changing Background Color */
     $('#music1').click(function() {
-        $('#audio').attr('src','audio/Butterfly Kiss.mp3');
-        $('.transition').css('background-color', 'blue');
+        musicColor('Ideal and the Real.mp3', 'blue');
     })
 
     $('#music2').click(function() {
-        $('#audio').attr('src','audio/Ideal and the Real.mp3');
-        $('.transition').css('background-color', 'yellow');
+        musicColor('Price.mp3', 'yellow');
     })
 
     $('#music3').click(function() {
-        $('#audio').attr('src','audio/Life Will Change.mp3');
-        $('.transition').css('background-color', 'red');
+        musicColor('Tokyo Daylight.mp3', 'lime');
     })
 
     $('#music4').click(function() {
-        $('#audio').attr('src','audio/Price.mp3');
-        $('.transition').css('background-color', 'green');
+        musicColor('', '#222222');
     })
 
-    $('#music5').click(function() {
-        $('#audio').attr('src','audio/Royal Days.mp3');
-        $('.transition').css('background-color', 'violet');
-    })
-
-    $('#music6').click(function() {
-        $('#audio').attr('src','audio/So Boring.mp3');
-        $('.transition').css('background-color', 'orange');
-    })
-
-    $('#music7').click(function() {
-        $('#audio').attr('src','audio/Tokyo Daylight.mp3');
-        $('.transition').css('background-color', 'lime');
-    })
-
-    $('#music8').click(function() {
-        $('#audio').attr('src',' ');
-        $('.transition').css('background-color', '#222222');
-    })
-
+    /* Adding Background Description */
     $('#backstory-btn').click(function() {
         backstory();
         $(this).remove();
     })
 
+    /* Adding Sounds for Button */
     $(':button').hover(function() {
-        var bleep = new Audio();
-        bleep.src = "audio/button.mp3"
-        bleep.play();
+        hoverSound();
     })
 
     $(':button').click(function() {
-        var select = new Audio();
-        select.src = "audio/select.mp3"
-        select.play();
+        selectSound();
     })
 
     var i = 0;
 
+    /* Adding Sounds for each item in Music Menu */
     for(i=1; i < 9; i++) {
         $('#music' + i).mouseenter(function() {
-            var bleep = new Audio();
-            bleep.src = "audio/button.mp3"
-            bleep.play();
+            hoverSound();
         })
 
         $('#music' + i).click(function() {
-            var select = new Audio();
-            select.src = "audio/select.mp3"
-            select.play();
+            selectSound();
         })
     }
 
+    /* Hide Music Menu */
     $('#music-menu').mouseleave(function() {
         $('#music-menu').hide();
     })
 
+    /* Adding Select Sound to Links in Side Menu */
     for(i=1; i < 6; i++) {
-        $('#link' + i).mouseenter(function() {
-            var bleep = new Audio();
-            bleep.src = "audio/button.mp3"
-            bleep.play();
-        })
-
         $('#link' + i).click(function() {
-            var select = new Audio();
-            select.src = "audio/select.mp3"
-            select.play();
+            selectSound();
         })
     }
 
+    /* Smoorh scrolling to Links within Page */
     var scrollLink = $('.scroll');
-
-    //Smoorh scrolling
     scrollLink.click(function(e) {
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top
         }, 500 )
     })
+
 })
 
+/* Parallax effect */
+$(window).scroll(function() {
+  parallax();
+})
+
+function parallax() {
+    var wScroll = $(window).scrollTop();
+    $(' .parallax--bg').css('background-position', 'center ' + (wScroll * 0.75) + 'px')
+}
+
+/* Background Music and Color */
+function musicColor(music, color) {
+    $('#audio').attr('src','audio/' + music);
+    $('.transition').css('background-color', color);
+}
+
+/* Backstory Description */
 function backstory() {
     $('#backstory-p').text('Nothing Special. Grew up in a city that is otherwise known as the Gulf Tiger or the City of Gold. Studied in a private school and achieved the top rank n class for a few years. And unfortunately, moved to the country refer to as Great White North.');
     $('#backstory-p').css('text-indent','2rem');
+}
+
+/* Select Sound */
+function selectSound() {
+   var select = new Audio();
+    select.src = "audio/select.mp3"
+    select.play();
+}
+
+/* Hover Sound */
+function hoverSound() {
+    var hover = new Audio();
+    hover.src = "audio/button.mp3"
+    hover.play();
 }
